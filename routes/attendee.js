9c1dd6541fe4a44acf9306126788b2a32d7ae49f
emmajb1004 <134862847+emmajb1004@users.noreply.github.com>
2025-12-15 20:49:34 +0000

    edits

--- routes/attendee.js
+++ routes/attendee.js
@@ -1,10 +1,11 @@
 // Create a new router
 const express = require("express")
 const router = express.Router()
+const BASE_PATH = '';
 
 const redirectLogin = (req, res, next) => {
         if (!req.session.userId ) {
-          res.redirect('/users/attendee/login') // redirect to the login page
+          res.redirect(`${BASE_PATH}/users/attendee/login`) // redirect to the login page
         } else { 
             next (); // move to the next middleware function
         } 
--- routes/organizer.js
+++ routes/organizer.js
@@ -1,10 +1,11 @@
 // Create a new router
 const express = require("express")
 const router = express.Router()
+const BASE_PATH = '';
 
 const redirectLogin = (req, res, next) => {
         if (!req.session.userId ) {
-          res.redirect('users/login') // redirect to the login page
+          res.redirect(`${BASE_PATH}/users/login`) // redirect to the login page
         } else { 
             next (); // move to the next middleware function
         } 
